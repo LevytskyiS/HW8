498bd3c62f3cbaac9623d79229eb6b9c0b71ad8f
LevytskyiS <levytskyi.sv@gmail.com>
2022-10-23 19:19:25 +0200

    Created the birthday.py

---
+++ birthdays.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+
+def main():
+
+    global users, current_datetime, delta
+    
+    current_datetime = datetime.now()
+    
+    delta = timedelta(days=7)
+    
+    users= [
+        {'name' : 'Bill', 'birthday' : datetime(year=2022, month=10, day=24)},
+        {'name' : 'John', 'birthday' : datetime(year=2022, month=10, day=24)},
+        {'name' : 'Kate', 'birthday' : datetime(year=2022, month=10, day=25)},
+        {'name' : 'Alisha', 'birthday' : datetime(year=2022, month=10, day=26)},
+        {'name' : 'Julie', 'birthday' : datetime(year=2022, month=10, day=27)},
+        {'name' : 'Sabina', 'birthday' : datetime(year=2022, month=10, day=28)},
+        {'name' : 'Anna', 'birthday' : datetime(year=2022, month=10, day=29)},
+        {'name' : 'Emma', 'birthday' : datetime(year=2022, month=10, day=30)},
+        {'name' : 'Charles', 'birthday' : datetime(year=2022, month=10, day=31)}
+    ]
+
+    get_birthdays_per_week(users)
+
+
+def get_birthdays_per_week(users):
+
+    names = []
+    day_of_week = []
+    result = defaultdict(list)
+
+    for nb in users:
+        name = nb.get('name')
+        birth = nb.get('birthday')
+        difference = birth - current_datetime
+
+        if difference <= delta and birth.strftime('%A') == 'Saturday':
+            birth = birth + timedelta(days=2)
+            names.append(name)
+            day_of_week.append(birth)
+
+        elif difference <= delta and birth.strftime('%A') == 'Sunday':
+            birth = birth + timedelta(days=1)
+            names.append(name)
+            day_of_week.append(birth)       
+        
+        elif difference <= delta:
+            names.append(name)
+            day_of_week.append(birth)
+
+    for d, n in zip(day_of_week, names):
+        result[d].append(n)
+   
+    for key, value in result.items():
+        lst_of_names = ', '.join(value)
+        print(f'{key.strftime("%A")}: {lst_of_names}')
+
+
+if __name__ == '__main__':
+    main()
+    quit()
